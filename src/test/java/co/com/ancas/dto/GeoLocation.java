5fbee9e9cd5bfd14526f2b0aa50383ed4d3b99b1
afcastro <afcastro@cotrafa.com.co>
2025-05-27 17:35:49 -0500

    Feat: Add configuration to use geolocation app

---
+++ src/test/java/co/com/ancas/dto/GeoLocation.java
@@ -0,0 +1,14 @@
+package co.com.ancas.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
+public class GeoLocation {
+    private double longitude;
+    private double latitude;
+}
